e5bf07eac90295f397f56a789bcdefc147a3d6be
evriyanaindrasaputra <eindrasap@gmail.com>
2020-04-15 20:54:07 +0700

    belajar vue pertama

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item>Fancy</b-nav-item>
+      <b-nav-item>Counter : {{ numCorrect }}/ {{ numTotal }}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["numCorrect", "numTotal"],
+};
+</script>
+
+<style scoped></style>
